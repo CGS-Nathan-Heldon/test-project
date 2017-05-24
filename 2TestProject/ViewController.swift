2343e5300e38e38d1836c2cd8306e44378bb0eb0
Nathan Heldon <Nathan@Nathans-MacBook-Pro-2.local>
2017-05-24 11:57:58 +1000

    Initial Commit

---
+++ 2TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2TestProject
+//
+//  Created by Nathan Heldon on 24/5/17.
+//  Copyright © 2017 Nathan Heldon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
