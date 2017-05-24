a5ace3418c0f3433f044f4075284809546eb2632
Nathan Heldon <Nathan@Nathans-MacBook-Pro-2.local>
2017-05-24 11:58:29 +1000

    Commit2

--- 2TestProject/ViewController.swift
+++ 2TestProject/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    print("MasterComment")
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
